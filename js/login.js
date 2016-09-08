20149ed0cd549913c0cea6b6059715d181afd8ad
Moer <199525807@qq.com>
2016-09-08 12:57:55 +0800

    增加了登录、注册页面的前端验证功能。

---
+++ js/login.js
@@ -0,0 +1,72 @@
+(function () {
+    var $loginBtn = $('#btn-login')
+    var $loginName = $('#login-username')
+    var $loginPassword = $('#login-password')
+    var $loginCode = $('#login-code')
+    var $registerBtn = $('#btn-register')
+    var $registerName = $('#register-username')
+    var $registerPassword = $('#register-password')
+    var $registerRepassword = $('#register-repassword')
+    var $registerCode = $('#register-code')
+
+    $('#to-register').click(function () {
+        $('.login').hide();
+        $('.register').show();
+    })
+    $('#to-login').click(function () {
+        $('.login').show();
+        $('.register').hide();
+    })
+
+    // 登录前端验证
+    $loginBtn.prop('disabled', true)
+    $('.login').on('keyup', 'input', function () {
+        var invaild = false;
+        var msg = ''
+        if ($loginName.val() == '') {
+            invaild = true
+            msg = '请输入用户名'
+        }
+        if ($loginPassword.val().length < 4 || $loginPassword.val().length > 12) {
+            invaild = true
+            msg = '密码不合法'
+        }
+        if ($loginCode.val() == '') {
+            invaild = true
+            msg = '请输入验证码'
+        }
+        if (msg === '') {
+            msg = '登录'
+        }
+
+        $loginBtn.attr({ disabled: invaild }).text(msg)
+
+    })
+
+    // 注册前端验证
+    $registerBtn.prop('disabled', true)
+    $('.register').on('keyup', 'input', function () {
+        var invaild = false;
+        var msg = ''
+        if ($registerName.val() == '') {
+            invaild = true
+            msg = '请输入用户名'
+        }
+        if ($registerPassword.val().length < 4 || $registerPassword.val().length > 12) {
+            invaild = true
+            msg = '密码不合法'
+        }
+        if ($registerRepassword.val() != $registerPassword.val()) {
+            invaild = true
+            msg = '两次输入密码不一致'
+        }
+        if ($registerCode.val() == '') {
+            invaild = true
+            msg = '请输入验证码'
+        }
+        if (msg === '') {
+            msg = '注册'
+        }
+        $registerBtn.attr({ disabled: invaild }).text(msg)
+    })
+})()
