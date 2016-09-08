96905d11d9ac6e155c9caf0909716cd3173887af
Moer <199525807@qq.com>
2016-09-08 19:57:02 +0800

    提交科普模块

---
+++ js/doyouknow.js
@@ -0,0 +1,47 @@
+$(function(){
+    // 绑定跳转的详情页
+    function bindDetails(url) {
+        $('body').css({ overflow: 'hidden' })
+        layer.open({
+            type: 2,
+            area: ['80%', '90%'],
+            fix: true,
+            title: '产品详情',
+            maxmin: true,
+            content: url,
+            end: function () {
+                $('body').css({ overflow: 'auto' })
+            }
+        });
+    }
+
+     // 获取产品列表数据
+    var $tpl = $('#tpl-doyouknow');
+
+    $.ajax({
+        type: "get",
+        url: "../json/doyouknow.json",
+        success: function (data) {
+            // 成功后展示产品列表
+            var tabPanes = $('.tab-content .tab-pane')
+
+            // 清空所有tab
+            for (var i = 0; i < tabPanes.length; i++) {
+                $(tabPanes[i]).empty();
+            }
+            $.each(data, function (index, el) {
+                var newtpl = $tpl.children().eq(0).clone()
+                newtpl.find('.title').text(el.title)
+                newtpl.find('.content').text(el.content)
+                newtpl.find('img').attr('src', el.image)
+                newtpl.find('.item').on('click', function (e) {
+                    e.preventDefault()
+                    e.stopPropagation()
+                    bindDetails($(this).attr('href'))
+                })
+                $(tabPanes[el.tab]).append(newtpl)
+                newtpl.show();
+            });
+        }
+    });
+})
