aaa8fe33af8ecbaf29b3b08600b5e008e40d4391
Moer <199525807@qq.com>
2016-09-08 21:00:19 +0800

    吐槽增加跟帖

--- js/details.js
+++ js/details.js
@@ -9,4 +9,23 @@ $(function() {
             src: src
         })
     })
+
+    $('body').on('click','.reply a',function(){
+        layer.open({
+            type: 1,
+            title: '回复评论',
+            closeBtn: 0,
+            area: '600px',
+            skin: 'layui-layer-rim', //边框
+            shadeClose: true,
+            content: $('#dialog-reply'),
+            btn: ['回复', '取消'], //按钮
+            btn1: function (index, layero) {
+                //按钮【按钮1】的回调
+            },
+            btn2: function (index, layero) {
+                //按钮【按钮2】的回调
+            }
+        });
+    })
 })
